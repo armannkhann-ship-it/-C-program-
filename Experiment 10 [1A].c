add856950e324cf55772978cf77c204aab48774e
Notredame41 <146503398+Notredame41@users.noreply.github.com>
2025-11-22 10:37:13 +0530

    Create Experiment 10 [1A].c

---
+++ Experiment 10 [1A].c
@@ -0,0 +1,29 @@
+/*
+Name:Ashaz Shakir Patel
+UIN:251M003
+Class:Mechanical FE
+Div:f
+*/
+#include <stdio.h>
+
+int main() {
+	int a, b, sum;
+	int *p1, *p2;
+
+	printf("Enter first number: ");
+	scanf("%d", &a);
+
+	printf("Enter second number: ");
+	scanf("%d", &b);
+
+
+	p1 = &a;
+	p2 = &b;
+
+
+	sum = *p1 + *p2;
+
+	printf("Sum = %d\n", sum);
+
+	return 0;
+}
