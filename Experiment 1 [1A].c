823b1035a3cf74fe1db0ed151f76f31c230437e3
armannkhann-ship-it <armannkhann@eng.rizvi.edu.in>
2025-11-24 19:02:03 +0530

    Update Experiment 1 [1A].c

--- Experiment 1 [1A].c
+++ Experiment 1 [1A].c
@@ -1,6 +1,6 @@
 /*
-Name: Ashaz Shakir Patel
-UIN: 251M003
+Name: CHOUDHARY ARMAN
+UIN: 251M030
 Class: FE Mechanical
 Div: F
 */
@@ -8,7 +8,7 @@ Div: F
 #include <stdio.h>
 
 int main() {
-    printf("Your name is Ashaz Shakir Patel\n");
+    printf("Your name is CHOUDHARY ARMAN\n");
     printf("Age: 18 yrs\n");
     printf("Class: FE Mechanical\n");
     printf("Division: F\n");
